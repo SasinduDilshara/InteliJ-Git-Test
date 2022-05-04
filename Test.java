c806abd0e5454aa757a6937dde2e568970eede49
SasinduDilshara <dilsharasasindu@gmail.com>
2022-05-04 23:06:08 +0530

    merge with trivial - 1

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi");
     }
-    public void abcd(){}
+    public void abcd2(){}
+    public void abcd3(){}
 }
